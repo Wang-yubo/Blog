4191693505cce22ebb403eb40f5b03e9aaf6086d
wangyubo <760478684@qq.com>
2020-05-20 00:02:51 +0800

    留言前端结束

---
+++ client/src/components/RichText.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <textarea id="demo" style="display: none;"></textarea>
+    <button type="button" class="layui-btn" @click="handleClick">提交留言</button>
+  </div>
+</template>
+
+<script>
+
+
+
+  export default {
+    name: "RichText",
+    data(){
+      return {
+        layedit : null,
+        index : 0
+      }
+    },
+    methods:{
+      handleClick(){
+        this.$emit("Sub",this.layedit.getContent(this.index));
+      }
+    },
+
+    mounted() {
+      // eslint-disable-next-line no-undef
+      layui.use('layedit', ()=>{
+        // eslint-disable-next-line no-undef
+        this.layedit = layui.layedit;
+        this.index = this.layedit.build('demo',{
+          tool : ['face']
+        }); //建立编辑器
+      });
+    }
+  }
+</script>
+
+<style scoped>
+</style>
